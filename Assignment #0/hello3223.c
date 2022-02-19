180e59824dffef335b5d6af015e7bcbd71586e5f
grxdiii <98719757+grxdiii@users.noreply.github.com>
2022-02-19 03:20:34 -0500

    Add files via upload

---
+++ Assignment #0/hello3223.c
@@ -0,0 +1,12 @@
+//  Gradi Tshielekeja Mbuyi
+//  COP 3223 Section 2
+//  Assignment #0 - Fin Aid / Hello World
+//
+//  The Program prints out the following "Hello 3223 class!"
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello 3223 class!\n");
+    return 0;
+}
